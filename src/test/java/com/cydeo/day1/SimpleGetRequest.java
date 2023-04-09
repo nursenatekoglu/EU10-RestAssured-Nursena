d45c355c9ebe81d3acb590951a9248866c2403ef
nursena <nursena.tekoglu@gmail.com>
2023-04-09 22:48:35 +0100

    Initial commit

---
+++ src/test/java/com/cydeo/day1/SimpleGetRequest.java
@@ -0,0 +1,28 @@
+package com.cydeo.day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+
+    String url = "http://52.91.189.215:8000/api/spartans";
+
+
+    @Test
+    public void test1(){
+
+        //send a request and save response inside the Response object
+        Response response = RestAssured.get(url);
+
+        //print response status code
+        System.out.println(response.statusCode());
+
+        //print response body
+        response.prettyPrint();
+
+    }
+
+
+}
