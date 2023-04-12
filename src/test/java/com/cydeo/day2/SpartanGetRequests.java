6e7d211479b7ecb26e99b46523e1dad368ba5fe1
nursena <nursena.tekoglu@gmail.com>
2023-04-12 02:29:12 +0100

    second test case is done

---
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -0,0 +1,103 @@
+package com.cydeo.day2;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SpartanGetRequests {
+
+
+    String baseUrl = "http://52.91.189.215:8000";
+
+
+    //    Given Accept type application/json
+    //    When user send GET request to api/spartans end point
+    //    Then status code must be 200
+    //    And response Content Type must be application/json
+    //    And response body should include spartan result
+
+
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when().get(baseUrl + "/api/spartans");
+
+        //printing status code from response object
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        //printing response content type from response object
+        System.out.println("response.contentType() = " + response.contentType());
+
+        //print whole result body
+        response.prettyPrint();
+
+
+
+        //how to do API testing then?
+
+        //verify status code is 200
+        Assertions.assertEquals(response.statusCode(),200);
+
+
+        //verify content type is application/json
+        Assertions.assertEquals(response.contentType(), "application/json");
+
+
+
+    }
+
+
+
+    //    Given Accept type application/json
+    //    When user send GET request to api/spartans/3
+    //    Then status code must be 200
+    //    And response content Type must be application/json
+    //    And response body should contain Fidole
+
+
+    @DisplayName("GET one spartan /api/spartans/3 and verify")
+    @Test
+    public void test2(){
+
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when().get(baseUrl + "/api/spartans/3");
+
+        System.out.println("response.contentType() = " + response.contentType());
+
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        response.prettyPrint();
+
+
+        //verify status code is 200
+        Assertions.assertEquals(response.statusCode(),200);
+
+
+        //verify content type is application/json
+        Assertions.assertEquals(response.contentType(), "application/json");
+
+
+        //verify the json body contains Fidole
+        Assertions.assertTrue(response.body().asString().contains("Fidole"));
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
