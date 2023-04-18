8c5b4602b114fe4f56147ec0e297bbf496f602a4
nursena <nursena.tekoglu@gmail.com>
2023-04-18 19:57:29 +0100

    api day 8 is done

---
+++ src/test/java/com/cydeo/utilities/SpartanAuthTestBase.java
@@ -0,0 +1,17 @@
+package com.cydeo.utilities;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.*;
+
+    public abstract class SpartanAuthTestBase {
+        @BeforeAll
+        public static void init(){
+            //save baseurl inside this variable so that we dont need to type each http method.
+            baseURI = "http://52.91.189.215:7000";
+
+        }
+    }
+
